fa9fcff6aaf41f9788042b6e6d1143605260a8c4
yunianshu <592908207@qq.com>
2022-07-04 10:26:20 +0800

    Initial commit

---
+++ app/src/main/java/iammert/com/view/scalinglayout/CollapsingDemoActivity.java
@@ -0,0 +1,25 @@
+package iammert.com.view.scalinglayout;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+/**
+ * Created by mertsimsek on 01/10/2017.
+ */
+
+public class CollapsingDemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_collapsing);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+}
---
+++ app/src/main/java/iammert/com/view/scalinglayout/MainActivity.java
@@ -0,0 +1,38 @@
+package iammert.com.view.scalinglayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        findViewById(R.id.demo1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, CollapsingDemoActivity.class));
+            }
+        });
+
+        findViewById(R.id.demo2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, FABDemo.class));
+            }
+        });
+
+        findViewById(R.id.demo3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SearchBarDemoActivity.class));
+            }
+        });
+    }
+}
